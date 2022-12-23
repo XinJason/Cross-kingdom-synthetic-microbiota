ab2e9ab4486ac713635d509afc216e87293b8d2b
Zhouxin5518 <zhouxin5518@163.com>
2022-12-23 11:34:11 +0800

    Update Tomato_Amplicon_sequence_Data_Process_pipeline.sh

--- Tomato_Amplicon_sequence_Data_Process_pipeline.sh
+++ Tomato_Amplicon_sequence_Data_Process_pipeline.sh
@@ -1,4 +1,5 @@
 [TOC]
+#Citation: Zhou et al., Cross-kingdom synthetic microbiota supports tomato suppression of Fusarium wilt disease. Nat Commun 13, 7890 (2022). https://doi.org/10.1038/s41467-022-35452-6
 #!/bin/bash
 # Xin Zhou
 
